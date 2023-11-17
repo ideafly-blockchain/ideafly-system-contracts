6b43adbbac044e35bae8c8db6a91b4182b0268c8
owen0x6f <owen0x6f@gmail.com>
2023-11-17 19:02:34 +0800

    add comments

--- contracts/Params.sol
+++ contracts/Params.sol
@@ -14,16 +14,25 @@ contract Params {
         IPunish(0x000000000000000000000000000000000000D002);
 
     // System params
-    uint16 public constant MaxValidators = 21;
 
+    // max active validators 
+    uint16 public constant MaxValidators = 21;
+    // margin threshold for a PoS type of validator
     uint public constant PosMinMargin = 5000 ether;
+    // margin threshold for a PoA type of validator
     uint public constant PoaMinMargin = 1 ether;
 
+    // The punish amount from validators margin when the validator is jailed
     uint public constant PunishAmount = 100 ether;
 
+    // JailPeriod: a block count, how many blocks a validator should be jailed when it got punished
     uint public constant JailPeriod = 86400;
+    // when a validator claim to exit, how many blocks its margin should be lock 
+    // (after that locking period, the valicator can withdraw its margin)
     uint public constant MarginLockPeriod = 403200;
+    // when a voter claim to withdraw its stake(vote), how many blocks its token should be lock
     uint public constant WithdrawLockPeriod = 86400;
+    // when a validator change its commison rate, it should take `PercentChangeLockPeriod` blocks to take effect
     uint public constant PercentChangeLockPeriod = 86400;
     uint constant PERCENT_BASE = 10000;
 
